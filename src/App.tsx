9058d403c30aa680b1e15ea9401b80d2417a1642
Anirudh Venkataraman <anirudhvenkat2010@gmail.com>
2025-07-14 21:07:27 -0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.tsx';
 import Contact from './components/contact.tsx';
 import About from './components/about.tsx';
-import Welcome from './components/Welcome.tsx';
 import Join from './components/Join.tsx';
 
 
